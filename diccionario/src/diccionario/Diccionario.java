9e0b3bf50ed04f3301e3c2ba9e29472915b46b37
whitehat-hacker551 <dcalinescu00@gmail.com>
2025-02-04 09:11:02 +0100

    Hola he cambiado
    Signed-off-by: whitehat-hacker551 <dcalinescu00@gmail.com>

---
+++ diccionario/src/diccionario/Diccionario.java
@@ -0,0 +1,11 @@
+package diccionario;
+
+public class Diccionario {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola, me llamo Davidddd");
+		System.out.println("Cambio 1");
+	}
+
+}
